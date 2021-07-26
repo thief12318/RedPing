26c92fe4b20ebf1c4600acda2158196bfb056a39
jbarinan <jbarinan@traxiontech.net>
2021-07-26 14:27:20 +0800

    almost

--- map.php
+++ map.php
@@ -1,76 +1,10 @@
 <?php
 
-  function add_location(){
-    $con=mysqli_connect ("localhost", 'root', '','redping');
-    if (!$con) {
-        die('Not connected : ' . mysqli_connect_error());
-    }
-    $location_id = $_GET['location'];
-    $user_id = $_GET['user'];
-    $time = $_GET['time'];
-    // Inserts new row with place data.
-    $query = sprintf("INSERT INTO my_pins " .
-        " (location_id, user_id, time) " .
-        " VALUES ('%s', '%s', '%s');",
-        mysqli_real_escape_string($con, $location_id),
-        mysqli_real_escape_string($con, $user_id),
-        mysqli_real_escape_string($con, $time));
+session_start();
 
-    $result = mysqli_query($con,$query);
-    echo json_encode("Inserted Successfully");
-    if (!$result) {
-        die('Invalid query: ' . mysqli_error($con));
-    }
-  }
+include 'mapdb.php';
+
   
-  function get_saved_locations(){
-    
-    $con=mysqli_connect ("localhost", 'root', '','redping');
-    if (!$con) {
-        die('Not connected : ' . mysqli_connect_error());
-    }
-    // update location with location_status if admin location_status.
-    $sqldata = mysqli_query($con,"select longitude,latitude from locations ");
-
-    $rows = array();
-    while($r = mysqli_fetch_assoc($sqldata)) {
-        $rows[] = $r;
-
-    }
-    $indexed = array_map('array_values', $rows);
-
-    //  $array = array_filter($indexed);
-
-    echo json_encode($indexed);
-    if (!$rows) {
-        return null;
-    }
-  }
-
-  function readings(){
-    
-    $con=mysqli_connect ("localhost", 'root', '','redping');
-    if (!$con) {
-        die('Not connected : ' . mysqli_connect_error());
-    }
-    // update location with location_status if admin location_status.
-    $sqldata = mysqli_query($con,"select reading from readings ");
-
-    $rows = array();
-    while($r = mysqli_fetch_assoc($sqldata)) {
-        $rows[] = $r;
-
-    }
-    $indexed = array_map('array_values', $rows);
-
-    //  $array = array_filter($indexed);
-
-    echo json_encode($indexed);
-    if (!$rows) {
-        return null;
-    }
-  }
-
 
 
 ?>
@@ -184,22 +118,47 @@
 
   <div id='map' style='height:100%;width:100%;'></div>
 
-  <div class="reading" style='
+  <div class="reading" id='reading' style='
   position:absolute; 
-  left: 650px; 
+  left: 800px; 
   top:80px; 
-  width: 180px;
-  height: 110px;
+  width: 220px;
+  height: 180px;
   background-color: white;
-  '>
-      <form action="" id="signupForm">
-          <h5>Street</h5>
-          <p>Readings: </p>
-          <input type="hidden" id="lat" name="lat" >
-          <input type="hidden" id="lng" name="lng">
+  '>  <h5><span id="streets"></span></h5>
+          <p>Reading: <span id="readings"></span></p>
 
-          <input type="submit" value="Pin" >
+
+      <form action="" id="signupForm">     
+         
+          <input type="hidden" id="location" name="location">
+          <input type="hidden" id="user" name="user" value="<?php echo  $_SESSION['user_id']; ?>">
+          
+          <div class="time_pin1" id='time_pin1'>
+              
+          <span>Select time:</span>
+          <select id="time1">
+            <?php for($x = 0 ;$x <  12; $x++){ ?>
+              <option value="<?php echo $x + 1; ?>"><?php echo $x + 1;?></option>
+            <?php } ?>
+          </select>
+          <select id="time2">
+            <option value="0" >AM</option>
+            <option value="120000">PM</option>
+          </select>
+
+            </div>
+
+          <div class="time_pin2" id='time_pin2'>
+          <span>Selected time:</span>
+          <span id="time_span1"></span>
+
+            </div>
+
+  
+          <input type="submit" id="pin" value="Pin" >
       </form>
+ 
   </div>
 
 
@@ -228,13 +187,7 @@
 
 $(function(){
     $("#reading").hide();
-    $("#").on("click", function(){
-        $("#div1, #div2").toggle();
-    });
 });
-
-
-
 
         mapboxgl.accessToken = 'pk.eyJ1IjoidGhpZWYxMjMxOCIsImEiOiJja3B1azZkbW0xYnB5MnVxY3Fva3ZxN3liIn0.AtpmrQgGaofQmeNWyMTp2Q'
 
@@ -256,6 +209,13 @@ $(function(){
     function setupMap(center){
 
        var saved_markers = <?= get_saved_locations() ?>;
+       var saved_streets = <?= streets() ?>;
+       var readings =  <?= readings() ?>;
+       var location_id =  <?= get_location_id() ?>;
+       var location_id_pin = <?= get_location_id_pin() ?>;
+       var user_id_pin = <?= get_user_id_pin() ?>;
+      var user = <?php if(isset($_SESSION['loggedin'])) {echo $_SESSION['user_id'];}else{ echo 0;} ?>;
+      var time_pin = <?= get_time_pin() ?>;
      
 
         var map = new mapboxgl.Map({
@@ -264,19 +224,23 @@ $(function(){
         center: center,
         zoom: 17
         })
+        
 
         var directions = new MapboxDirections({
           accessToken: mapboxgl.accessToken
         })
 
         var popup = new Array(saved_markers.length);
-
+        var markers = new Array(saved_markers.length);
+        
     
-       var readings =  <?= readings() ?>;
+      
 
         var counter = 1;
-   
 
+       
+     
+      
         for (let i = 0; i < saved_markers.length; i++) {
           popup[i] = new mapboxgl.Popup({ 
             offset: 20, 
@@ -289,10 +253,11 @@ $(function(){
             );
             counter++;
         }
+  
         
 
       
-        
+          var der = 0;
 
        map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
      //  map.addControl(directions, 'bottom-left')
@@ -301,31 +266,111 @@ $(function(){
         for (let i = 0; i < saved_markers.length; i++) {
           var el = document.createElement('div');
           el.className = 'marker';
-          var markerss = new mapboxgl.Marker(el)
-           .setLngLat(saved_markers[i])
-           .setPopup(popup[i],top) 
-           .addTo(map)
-        }
+          var change = 0;
+          for (let y = 0; y < location_id_pin.length; y++){
+              if(Number(location_id[i]) ==  Number(location_id_pin[y]) && Number(user_id_pin[y]) == user){
+              change = 1;
+              }
+          }
+          if(change == 0){
+          markers[i] = new mapboxgl.Marker(el)
+          .setLngLat(saved_markers[i])
+                .setPopup(popup[i],top) 
+                .addTo(map);
+           
+          }else{
+            markers[i] = new mapboxgl.Marker()
+            .setLngLat(saved_markers[i])
+                .setPopup(popup[i],top) 
+                .addTo(map);
+           
+          }    
+          
+         
+           markers[i].getElement().addEventListener('click', () => {
+            //var lngLat = markers[i].getLngLat();
+            
+            der = 1;
 
-        markers.on('click', function (e) {
-        window.alert('test');
-
-
+            document.getElementById("streets").innerHTML = saved_streets[i];
+            document.getElementById("readings").innerHTML = readings[i];
+            document.getElementById("location").value = location_id[i];
+           
+            
+          //  document.getElementById("lat").value = lngLat.lat;
+           // document.getElementById("lng").value = lngLat.lng;
+        
+            for (let x = 0; x < location_id_pin.length; x++){
+              if(Number(location_id[i]) ==  Number(location_id_pin[x]) && Number(user_id_pin[x]) == user){
+                document.getElementById("pin").value = "Remove";
+                document.getElementById("time_span1").innerHTML = time_pin[x];
+                $("#time_pin1").hide();
+                $("#time_pin2").show();
+                alert(user_id_pin[y])
+                break;
+              }else{
+                $("#time_pin1").show();
+                $("#time_pin2").hide();
+                document.getElementById("pin").value = "Pin";
+               
+              
+              }
+            }
+           
+            
         });
+        
 
 
-   
         
-     
-        
+        }
 
 
       
+        
+        map.on('load', function() {
+        // window.alert(location_id);
+      
+        });
+        
+        map.on('click', function (e) {
+          //  $("#reading").show();
+          
+            if(der == 0){
+              $("#reading").hide();
+            }else{
+              $("#reading").show();
+              der = 0;
+            }
 
+        });
 
+        $('#signupForm').submit(function(event){
+          <?php if(isset($_SESSION['loggedin'])) { ?>
+            event.preventDefault();
+            var location_id = $('#location').val();
+            var user_id = $('#user').val();
+            var time1 = $('#time1').val();
+            var time2 = $('#time2').val();
 
+            var url = 'mapdb.php?add_location&location=' + location_id 
+            + '&user=' + user_id + '&time1=' + time1
+            +  '&time2=' + time2;
+            $.ajax({
+                url: url,
+                method: 'GET',
+                dataType: 'json',
+                success: function(data){
+                    alert(data);
+                    location.reload();
+                }
+               
+            });
+        <?php  }else{ ?> alert("You need to loggin first!");  <?php } ?> 
+         
+        });
+        
       }
-
 
   </script>
 
